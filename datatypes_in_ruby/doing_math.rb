ea7d881d30bf7772beef050ea6b5c69b871dcf8f
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-11-18 17:04:21 -0600

    practice with naming convention for declaring variables and using variables in doing math

--- datatypes_in_ruby/doing_math.rb
+++ datatypes_in_ruby/doing_math.rb
@@ -6,3 +6,15 @@ puts 2**4
 puts 20 % 2
 
 # prints out 12, 7, 15, 3, 16, and 0.
+
+sum = 13 + 379
+puts sum
+# 392
+
+product = 932 * 15
+puts product
+# 13845
+
+quotient = 13209 / 17
+puts quotient
+# 777
---
+++ datatypes_in_ruby/naming_conventions.rb
@@ -0,0 +1,9 @@
+# do not create variables with capital letters and no spaces, use underscore instead.
+# capital letters at the beginning can cause confusion because they are used for specific things
+name = "Evelyn"
+puts name
+
+my_name = "Evelyn"
+my_age = 26
+puts my_name
+puts my_age
