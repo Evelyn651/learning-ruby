f4f92dffccf4a7ba11b7ee3d45926ce79619971a
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-11-18 16:56:32 -0600

    learning built-in methods, single line, and multiline comments

---
+++ datatypes_in_ruby/methods.rb
@@ -0,0 +1,20 @@
+'I love espresso'.length
+# 15 characters
+#
+'Evelyn Ceci Acosta'.length
+# 18 characters
+#
+'Evelyn Ceci Acosta'.reverse
+# atsocA iceC nylevE
+
+puts "evelyn".upcase
+# EVELYN
+
+puts "evelyn".downcase
+# evelyn
+
+=begin
+multiline comments are used with begin and end words with an equals sign
+to indicate the beginning and end
+=end
+
