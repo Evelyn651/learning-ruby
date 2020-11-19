3fa182126b9caca54f57f6cb294b3167a5174d77
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-11-19 15:38:29 -0600

    first small program with user input and output with gets built in function and learned what chomp does and the importance of ! next to the built in methods.

--- datatypes_in_ruby/small_project_with_input_output.rb
+++ datatypes_in_ruby/small_project_with_input_output.rb
@@ -2,6 +2,8 @@ print "What's your first name? "
 first_name = gets.chomp
 first_name.capitalize!
 # i'm thinking gets is a built in function that takes whatever the user inputs.
+# chomp gets rid of the extra blank line that is created when using the built function gets
+#this small program can also get numbers and characters like whats used when inputing a phone number and email.
 
 print "What's your last name? "
 last_name = gets.chomp
