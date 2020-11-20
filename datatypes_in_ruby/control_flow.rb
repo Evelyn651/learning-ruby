05443d887829335d20af2c7e584168ed793fceb4
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-11-20 16:50:54 -0600

    first original if elsif statment and got my projects to run within IntelliJ.

--- datatypes_in_ruby/control_flow.rb
+++ datatypes_in_ruby/control_flow.rb
@@ -1,2 +1,10 @@
 # if else statements with multiple conditions have elsif instead of else if
+# first original if statement
 
+num = 45
+
+if num < 10
+  print "#{num} is less than 10"
+elsif num > 10
+  print "#{num} is greater than 10"
+end
