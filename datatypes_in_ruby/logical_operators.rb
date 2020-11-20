9df0e2e5835731fb1d492d0a77e0236943ecb5e5
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-11-20 17:48:17 -0600

    need more practice to understand unless

--- datatypes_in_ruby/logical_operators.rb
+++ datatypes_in_ruby/logical_operators.rb
@@ -3,3 +3,8 @@
 # || is the same as well where everything evaluates as true except for when both sides of the operator are false
 
 # the use of ! to imply not is the same
+
+sleepy = false
+  print "Get some coffee!"
+unless sleepy
+end
