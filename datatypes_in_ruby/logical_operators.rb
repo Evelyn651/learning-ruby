4d9fe769b263a6f915a12b4e7f802e03d587a0fd
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-11-20 17:33:00 -0600

    just notes on the similarity of operators between java and ruby

---
+++ datatypes_in_ruby/logical_operators.rb
@@ -0,0 +1,5 @@
+# && is the same as java where both sides of the && has to be true otherwise its false
+
+# || is the same as well where everything evaluates as true except for when both sides of the operator are false
+
+# the use of ! to imply not is the same
