eeaa778d0110e2853d0542865fabe575cdc0b908
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-11-20 17:07:56 -0600

    practice with unless in if statement.

--- datatypes_in_ruby/control_flow.rb
+++ datatypes_in_ruby/control_flow.rb
@@ -1,10 +1,20 @@
 # if else statements with multiple conditions have elsif instead of else if
 # first original if statement
 
-num = 45
-
+num = 10
 if num < 10
   print "#{num} is less than 10"
 elsif num > 10
   print "#{num} is greater than 10"
+else num == 10
+     print "#{num} is equal to 10"
+end
+
+#---------------the use of unless----------------------------
+
+
+unless false
+  puts 'Time to eat!'
+else
+  puts "I'm writing Ruby programs!"
 end
