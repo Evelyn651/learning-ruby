88c80dc124eff5ba333e63eed2aede2fc7237e43
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-11-20 20:24:57 -0600

    learning the ruby format for for loops using inclusive and exclusive ranges

---
+++ datatypes_in_ruby/inclusive_and_exclusive_for_loops.rb
@@ -0,0 +1,6 @@
+for num in 1..15
+  puts num
+end
+
+# new format for for loops using the word in and .. or ... print out the numbers from 1 to 15, with .. it includes 15
+# with ... it excludes 15. it iterates automatically without a need for an expression like num += 1.
