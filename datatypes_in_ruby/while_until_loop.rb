db82825c74ef64dae11baf2b8571f5917a5e72a1
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-11-21 23:33:49 -0600

    more practice with until in while loop but my while loop with the var num wont print out

--- datatypes_in_ruby/while_until_loop.rb
+++ datatypes_in_ruby/while_until_loop.rb
@@ -1,7 +1,16 @@
-counter = 1
-until counter > 10
-  puts counter
-  counter += 1
-end
+# counter = 1
+# until counter > 10
+#   puts counter
+#   counter += 1
+# end
 
 # ruby does not use ++ or -- to increment or decrement numbers. Have to use +=, -=, *=, or /=
+
+# more practice with until in a while loop
+#
+#
+num = 1
+until num <= 50 do
+  print num
+  num += 1
+end
