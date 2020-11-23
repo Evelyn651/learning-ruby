96c21df93b4f9cd4dc608df8f2418612090e3196
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-11-22 23:07:42 -0600

    learning about multidimensional arrays - arrays within arrays within arrays

---
+++ datatypes_in_ruby/arrays_of_arrays.rb
@@ -0,0 +1,6 @@
+# arrays can be multidimensional meaning they can hold arrays within arrays
+#
+
+multi_d_array = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+multi_d_array.each { |x| puts "#{x}\n"}
