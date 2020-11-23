abc522ad17af8e582f9c56c61bc1c80a9f903e51
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-11-22 23:12:26 -0600

    learning about hashes and how to use them

---
+++ datatypes_in_ruby/hashes.rb
@@ -0,0 +1,10 @@
+my_hash = { "name" => "Eric",
+            "age" => 26,
+            "hungry?" => true
+}
+
+puts my_hash["name"]
+puts my_hash["age"]
+puts my_hash["hungry?"]
+
+# hashes behave like kay-value pairs
