1bb9299d7135bd3c83a8ef2027529b47f4ef06e2
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-11-22 22:37:32 -0600

    practice with the built in method .split

---
+++ datatypes_in_ruby/split_built_in_method.rb
@@ -0,0 +1,10 @@
+puts "Enter a salutation"
+text = gets.chomp
+
+puts redact = gets.chomp
+
+# declare a var called words and set it equal to the result of text.split  then pass " " through .split to get an
+# array made up of the words from the var text
+#
+
+words = text.split(" ")
